38c406ef4500a55f914261dd7fdd14db7a57c2a9
Lucasjk98 <64111906+Lucasjk98@users.noreply.github.com>
2024-05-16 16:04:03 -0600

    feat: initialize backend serverless with express (#2)
    * set up backend services app template
    
    * add serverless offline plugin
    
    * set up serverless http library and express
    
    * add .env support, create env, and add env to gitignore
    
    * integrate express
    
    * fix serverless.yml api setup

---
+++ apps/services/app.ts
@@ -0,0 +1,9 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports.handler = serverless(app);
