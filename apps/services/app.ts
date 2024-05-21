658d9379f37643e243fcf226241696519c424eea
Lucas Jimenez <lucasjimenezk1998@gmail.com>
2024-05-20 21:04:21 -0600

    connect DB

--- apps/services/app.ts
+++ apps/services/app.ts
@@ -1,9 +1,36 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const connectToDatabase = require('./config/database'); // Import your database config
 const app = express();
+const User = require('./models/User');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  try {
+    await connectToDatabase();  // Ensure DB connection is ready
+    res.send('Connected to DB successfully.');
+  } catch (err) {
+    res.status(500).send('Database connection failed');
+  }
+}); 
+
+
+
+
+app.post('/users', async (req, res) => {
+  try {
+    await connectToDatabase();
+    const user = new User(req.body);
+    const savedUser = await user.save();
+    res.status(201).send(savedUser);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
+
 
 module.exports.handler = serverless(app);
